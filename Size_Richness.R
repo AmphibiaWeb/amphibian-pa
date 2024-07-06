4918749b9dc273e700f56ee03b07eb4ad8a74e03
steigeec <40217381+steigeec@users.noreply.github.com>
2024-07-06 12:11:45 -0700

    Update Size_Richness.R

--- Size_Richness.R
+++ Size_Richness.R
@@ -1,37 +1,39 @@
-#Size_Richness.R                                                                                                                                                            #
-#author: Emma Steigerwald                                                                                                                                                   #
-#############################################################################################################################################################################
+# R language
+
+# Plot of amphibian species richness based on PA size and analysis of how species area is impacted by PA characteristics
 
 #Set up working environment
-setwd("G:/Shared drives/AW-Data/ConservationWG/ConservationWG_GithubRepo/Data")
-#Load libraries
-library(dplyr) #yes
-library(ggplot2) #yes
-library(plyr) #yes
+library(dplyr)
+library(ggplot2) 
+library(plyr) 
 library(hexbin)
 library(RColorBrewer)
-library(stringr) #yes
-library(cowplot) #NO
+library(stringr)
+library(cowplot)
 library(SciViews)
-library(tidyr) #yes
+library(tidyr) 
 library(ggpattern)
 library(forcats)
-library(khroma) #Maybe use https://www.rdocumentation.org/packages/ggsci/versions/2.9 instead?
+library(khroma)
 muted <- colour("muted")
 library(lessR)
-library(data.table) #yes
+library(data.table)
 library(scales)
 library(extrafont)
 font_import()
+library(car)
+library(MASS)
+library(lmtest)
+library(pscl)
 
 #####################
+
 #Load all species found in all PAs
 SpPA <- read.csv("Data using Amphibian Ranges for Species Richness/All_Amphibia with WDPA one to many.csv")
 
-#Associate a threat level with these amphibians
+# Associate a threat level with these amphibians
 Amph <- read.csv("Data using Amphibian Ranges for Species Richness/Amphibians_w_ranges.csv")
-length(unique(Amph$binomial))   #7094 species
-#Here's the threat data. let's clean itup.
+length(unique(Amph$binomial))   
 Amph$code[Amph$code==""] <- "In assessment"
 Amph$code[Amph$code=="V" | Amph$code=="VU-provisional"] <- "VU"
 Amph$code[Amph$code=="NT-provisional" | Amph$code=="NT-Provisional"] <- "NT"
@@ -49,39 +51,38 @@ SpPA <- left_join(Amph, SpPA, by="binomial")
 
 #Label amphibians not on PAs as "not protected"
 SpPA$WDPA_PID[SpPA$WDPA_PID == ""] <- "NotProtected"
+
+# Count amphibian species per PA
 SpPaSum <- SpPA %>%
   dplyr::group_by(WDPA_PID) %>%
   dplyr::summarise(totRichness = length(unique(binomial))) 
 
+# Count threatened amphibians per PA per IUCN threatened category
 ThreatSum <- SpPA %>%
   dplyr::group_by(WDPA_PID, AWiucn) %>%
-  dplyr::summarise(count = length(unique(binomial)))     
+  dplyr::summarise(count = length(unique(binomial)))    
 #Label the catch all for amphibians not on PAs
 ThreatSum$WDPA_PID[ThreatSum$WDPA_PID == ""] <- "NotProtected"
+
 #Make this horizontal format
 ThreatSum <- ThreatSum %>% spread(AWiucn, count)
 SpPaSum <- left_join(SpPaSum, ThreatSum, by=c("WDPA_PID"="WDPA_PID"))
+
 #Calculate total number threatened species
 SpPaSum$AllThreat <- rowSums(SpPaSum[, c('CR', 'EN', 'EW', 'EX', 'NT', 'VU')], na.rm=TRUE) 
-#Total species assessed (not DD or in assessment
+#Total species assessed (not DD or in assessment)
 SpPaSum$AllAssessed <- rowSums(SpPaSum[, c('CR', 'EN', 'EW', 'EX', 'NT', 'VU', 'LC')], na.rm=TRUE) 
 #Total proportion threatened species
-SpPaSum$ThreatProp <- SpPaSum$AllThreat/SpPaSum$AllAssessed
+SpPaSum$ThreatProp <- SpPaSum$AllThreat/SpPaSum$AllAssessed              
 
-#SAVE SpPA for the cumulative amphibian species covered, below!!!
-#Now we've gotten what we want fromt his crazy large table, remove it.
-remove(SpPA)                 
-
-#####################
-#Finally, import the country, continent, hemisphere info
+# Import the country, continent, hemisphere info
 xtra <- read.csv("World_Database_Protected_Areas/WDPA_all.csv")
 names(xtra)[names(xtra) == "NAME_EN"] <- "Country"
 names(xtra)[names(xtra) == "ï..WDPA_PID"] <- "WDPA_PID"
 xtra <- xtra[ , which(names(xtra) %in% c("WDPA_PID", "CONTINENT", "Country"))]
 xtra <- unique(xtra)
 
-#Now bring in the WDPA data from 
-###WE STILL NEED TO FIX ENCODING ISSUES HERE!!!
+# Bring in the WDPA data
 wdpa <- read.csv("World_Database_Protected_Areas/WDPA_WDOECM_Jul2021_Public_all_csv/WDPA_WDOECM_Jul2021_Public_all_csv.csv")
 #We need to take out marine area from what we're considering as protected area size!
 wdpa$GIS_T_AREA <-wdpa$GIS_AREA - wdpa$GIS_M_AREA
@@ -90,9 +91,7 @@ wdpa$STATUS_YR <- as.numeric(wdpa$STATUS_YR)
 #Remove blank rows. 
 wdpa <- wdpa[rowSums(is.na(wdpa)) != ncol(wdpa),]
 
-
 #Now, merge...
-#THIS IS ALL WDPAs WHETHER THEY HAVE AMPHIBIANS OR NOT! 
 SpPaSum <- left_join(SpPaSum, wdpa, by=c("WDPA_PID"="WDPA_PID"))
 SpPaSum <- left_join(SpPaSum, xtra, by=c("WDPA_PID"="WDPA_PID"))
 #Remove extra columns
@@ -103,14 +102,9 @@ SpPaSum <- SpPaSum[ , -which(names(SpPaSum) %in% torem)]
 RHT_WDPA <- SpPaSum[SpPaSum$totRichness > 0,]
 #Remove blank rows
 RHT_WDPA <- RHT_WDPA[rowSums(is.na(RHT_WDPA)) != ncol(RHT_WDPA),]
-
-#Get rid of everything we don't need, because it burdens the system
-remove(xtra, wdpa, torem, SpPaSum, threatdata, ThreatSum, gdp)
-
 #replace continent names in all to clarify
 RHT_WDPA <- as.data.frame(lapply(RHT_WDPA ,function(x) if(is.character(x)|is.factor(x)) gsub("Oceania","Australia and Oceania", x) else x))
 RHT_WDPA <- as.data.frame(lapply(RHT_WDPA ,function(x) if(is.character(x)|is.factor(x)) gsub("North America","North and Central America", x) else x))
-
 #Remove records that do not seem to correspond to established protected areas, including those with their area name 
 #recorded as “area not protected” (2599 polygons), or their status recorded as “degazetted,”“proposed,”“recommended,”
 #“in preparation,” or “unset” (1024 polygons and 1751 points). Check for them by manually revising the search results of
@@ -118,20 +112,15 @@ RHT_WDPA <- as.data.frame(lapply(RHT_WDPA ,function(x) if(is.character(x)|is.fac
 RHT_WDPA %>% filter_all(any_vars(grepl("not protected|degazetted|proposed|recommended|in preparation|unset",.)))
 #Filter out PID 555564362 and 305921, which are both "proposed" rather than created it looks like  
 RHT_WDPA <- RHT_WDPA[!(RHT_WDPA$WDPA_PID=="555564362" | RHT_WDPA$WDPA_PID=="305921"),]
-#Dataframe shrinks from 137,660 to 137,658;   so this worked as expected!!                   
-                   
-#####################################################################################
-#ALL QUESTIONS HAVING TO DO WITH PA AREA
-#####################################################################################
 
+#Get rid of everything we don't need, because it burdens the system
+remove(SpPA, xtra, wdpa, torem, SpPaSum, threatdata, ThreatSum, gdp)
+                                
 #Get rid of any NA values in columns of interest
 AR <- RHT_WDPA[RHT_WDPA$GIS_T_AREA != 0, ]
 AR <- AR[!is.na(AR$GIS_T_AREA), ]
 
-################
-# Histogram of PA area
-
-#Hexbin of the same image
+# Hexbin of PA area by species richness
 jpeg("../Figures/PAsize-SpeciesRichness_HexBin(ln).jpg", width = 2000, height = 2000, res=100, quality = 100)
 ggplot(data=AR, aes(x=GIS_T_AREA, y=totRichness, width=1)) +
   stat_binhex(aes(fill=..count..), bins=75, colour="black", size=0.01) + 
@@ -157,102 +146,96 @@ ggplot(data=AR, aes(x=GIS_T_AREA, y=totRichness, width=1)) +
                      limits=c(1,10^5))  + 
   scale_y_continuous(expand = c(0.008,0.008))
 dev.off()   
-                   
-#Display this as a regression?
-jpeg("../Figures/PAsize-SpeciesRichness_Regression.jpg", width = 2000, height = 2000, res=100, quality = 100)
-ggplot(data=AR, aes(x=GIS_T_AREA, y=totRichness, width=1)) +
-  geom_point(size=0.00000000000001)+
-  geom_smooth(method='lm', color="black", lw=2, fill="grey") +                 
-  labs(title="How PA size relates to species richness", y = "Species richness", x = "PA size (km^2)") +
-  theme(axis.text.x = element_text(angle=45, size=45, vjust=0.5), axis.text.y = element_text(size=45), axis.title=element_text(size=45), 
-        plot.title=element_blank(), legend.text=element_text(size=40), legend.title=element_text(size=45), legend.key.height = unit(3, "cm"), 
-        legend.key.width = unit(1.5,"cm"), panel.background = element_rect(fill = "white", colour = "grey50"), strip.text = element_text(size=45)) +
-  scale_x_continuous(limits = c(0, 10000), expand = c(0.008, 0.008), breaks=seq(0, 10000, by=1000), labels=seq(0, 10000, by=1000)) + 
-  scale_y_continuous(limits = c(0,180), expand = c(0.008, 0.008))
-dev.off()  
+                                  
+######################################################################
 
+# Regression analyses modelling impact of PA characteristics on amphibian species richness                     
                      
-#Test assumptions of linear regression:
-
-model <- lm(totRichness ~ log(GIS_T_AREA), data=AR)   
-model_1 <- lm(totRichness ~ log(GIS_T_AREA) + CONTINENT, data=AR)
+# For regressions and tests, I want to include latitude as a factor:
+directory_path <- "your/path/WDPA Shapefiles with Country Name/"
+csvs <- list.files(directory_path, pattern = "\\.csv$", full.names = TRUE)                     
+# Initialize an empty data frame to store the combined data
+combined_data <- data.frame()
+# Read and rbind each CSV file
+for (csv_file in csvs) {
+  current_data <- read.csv(csv_file)
+  combined_data <- rbind(combined_data, current_data)  
+  # Remove the original object to save memory
+  rm(current_data)
+}                    
+combined_data$AvgLat<-  abs(round(combined_data$AvgLat, 2))  
+AR <- na.omit(AR, cols = "WDPA_PID")
+combined_data <- na.omit(combined_data)
+# merge as data.tables,  as R is better able to deal with very large ones.                      
+setDT(AR)
+setDT(combined_data)                    
+AR <- merge(AR, combined_data, by.y = "Name", by.x = "WDPA_PID", all.x = TRUE)
                      
-#   Coefficients:
-# (Intercept)   GIS_T_AREA  
-# 11.9817774    0.0009729     
-
-library(broom)                     
-model.diag.metrics <- augment(model)
-jpeg("../Figures/model.jpg")
-par(mfrow = c(2, 2))
+# Clean up unneeded data and columns                     
+rm(list=c("Amph", "RHT_WDPA", "csvs", "csv_file", "directory_path", "keep", "muted"))                    
+AR<-AR[,c("WDPA_PID", "totRichness", "GIS_T_AREA", "IUCN_CAT", "STATUS_YR")]  
+set.seed(123)  # Set seed for reproducibility
+subset_AR <- AR[sample(nrow(AR), 10000), ]# Randomly sample 10,000 rows
+                     
+#Test assumptions of linear regression (using a subset of the data):
+model <- lm(totRichness ~ log(GIS_T_AREA), data=subset_AR)   
+model_1 <- lm(totRichness ~ log(GIS_T_AREA)*STATUS_YR + AvgLat, data=subset_AR)                   
 plot(model)
-dev.off()
+plot(model_1)                    
+                  
+# Try Poisson family GLM (since species richness is count data)                    
+model <- glm(totRichness ~ log(GIS_T_AREA), data = subset_AR, family = poisson)
+model_1 <- glm(totRichness ~ log(GIS_T_AREA)*STATUS_YR + AvgLat, data = subset_AR, family = poisson)                                       
+# explore deviance residulas, which should follow roughly normal distribution:
+plot(model, which = 1)    # No, does not look normal
+# ratio of the deviance to its degrees of freedom can be compared to a chi-square distribution. In a well-specified model, this ratio should be close to 1.
+pchisq(model$deviance, df = model$df.residual, lower.tail = FALSE)     # No, this in fact is close to 0  
+plot(model_1, which = 1)
+pchisq(model_1$deviance, df = model_1$df.residual, lower.tail = FALSE)                       
                      
-jpeg("../Figures/model_1.jpg")
-par(mfrow = c(2, 2))
-plot(model_1)
-dev.off()
+# Since we've got overdispersion, let's do negative binomial instead                     
+model <- glm.nb(totRichness ~ log(GIS_T_AREA), data = subset_AR)
+model_1 <- glm.nb(totRichness ~ log(GIS_T_AREA)*STATUS_YR + AvgLat, data = subset_AR)                     
+plot(model, which = 1)    
+pchisq(model$deviance, df = model$df.residual, lower.tail = FALSE)     # 
+plot(model_1, which = 1)    
+pchisq(model_1$deviance, df = model_1$df.residual, lower.tail = FALSE)                        
+# Definitely the closest to a fit that we've gotten.  
                      
-#The diagnostic plots show residuals in four different ways:
+# Let's compare full models' AIC
+AR <- AR[!(AR$IUCN_CAT %in% c("Not Applicable", "Not Assigned", "Not Reported")), ]
 
-#(1) Residuals vs Fitted. Used to check the linear relationship assumptions. A horizontal line, without distinct patterns is an indication for a linear relationship, what is good.
-#violated
-#(2) Normal Q-Q. Used to examine whether the residuals are normally distributed. It’s good if residuals points follow the straight dashed line.
-#violated
-#(3) Scale-Location (or Spread-Location). Used to check the homogeneity of variance of the residuals (homoscedasticity). Horizontal line with equally spread points is a good indication of homoscedasticity. This is not the case in our example, where we have a heteroscedasticity problem.
-#violated
-#(4) Residuals vs Leverage. Used to identify influential cases, that is extreme values that might influence the regression results when included or excluded from the analysis. This plot will be described further in the next sections.                     
-#violated 
-                     
-summary(model)
-#Call:
-#lm(formula = totRichness ~ log(GIS_T_AREA), data = AR)
-#Residuals:
-#    Min      1Q  Median      3Q     Max 
-#-21.979  -6.228  -2.373   3.435 187.603 
-#
-#Coefficients:
-#                 Estimate Std. Error t value            Pr(>|t|)    
-#(Intercept)     12.616364   0.022415   562.8 <0.0000000000000002 ***
-#log(GIS_T_AREA)  0.917902   0.006875   133.5 <0.0000000000000002 ***
-#---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-                     
-#Residual standard error: 10.94 on 245356 degrees of freedom
-#Multiple R-squared:  0.06773,	Adjusted R-squared:  0.06772 
-#F-statistic: 1.782e+04 on 1 and 245356 DF,  p-value: < 0.00000000000000022                     
-                     
-summary(model_1)
-#Call:
-#lm(formula = totRichness ~ log(GIS_T_AREA) + CONTINENT, data = AR)
+# Full models of interest
+# We imagine area and year might interact, in that pehaps the differences between big and small PAs only emerge over time                     
+null <-  glm.nb(totRichness ~ 1, data = AR)                    
+model <- glm.nb(totRichness ~ AvgLat, data = AR)                     
+model_0 <- glm.nb(totRichness ~ log(GIS_T_AREA), data = AR)
+model_1 <- glm.nb(totRichness ~ log(GIS_T_AREA) + AvgLat, data = AR)   
+model_2 <- glm.nb(totRichness ~ log(GIS_T_AREA) + STATUS_YR, data = AR)   
+model_3 <- glm.nb(totRichness ~ log(GIS_T_AREA):STATUS_YR, data = AR) 
+model_4 <- glm.nb(totRichness ~ log(GIS_T_AREA):STATUS_YR + STATUS_YR, data = AR) 
+model_5 <- glm.nb(totRichness ~ log(GIS_T_AREA):STATUS_YR + log(GIS_T_AREA), data = AR) 
+model_6 <- glm.nb(totRichness ~ log(GIS_T_AREA)*STATUS_YR, data = AR)                      
+model_7 <- glm.nb(totRichness ~ log(GIS_T_AREA):STATUS_YR + AvgLat, data = AR)        
+model_8 <- glm.nb(totRichness ~ log(GIS_T_AREA):STATUS_YR + AvgLat + IUCN_CAT, data = AR) 
+model_9 <- glm.nb(totRichness ~ log(GIS_T_AREA)*STATUS_YR + AvgLat + IUCN_CAT, data = AR)                      
 
-#Residuals:
-#    Min      1Q  Median      3Q     Max 
-#-48.075  -4.426  -1.249   4.217 176.463 
+# model comparison by AIC                     
+AIC_values <- c(AIC(null), AIC(model), AIC(model_0), AIC(model_1), AIC(model_2), AIC(model_3), AIC(model_4), AIC(model_5), AIC(model_6), AIC(model_7), AIC(model_8), AIC(model_9))
+print(AIC_values)                
 
-#Coefficients:
-                                                      Estimate Std. Error  t value            Pr(>|t|)    
-#(Intercept)                                          27.998320   0.103555  270.371 <0.0000000000000002 ***
-#log(GIS_T_AREA)                                       0.407576   0.006335   64.341 <0.0000000000000002 ***
-#CONTINENTAsia                                       -14.970802   0.133063 -112.509 <0.0000000000000002 ***
-#CONTINENTAustralia and New Zealand                  -20.188033   0.122152 -165.270 <0.0000000000000002 ***
-#CONTINENTCanada and U.S.A.                          -14.865769   0.118704 -125.234 <0.0000000000000002 ***
-#CONTINENTCentral America, Mexico, and the Caribbean -11.276719   0.192181  -58.678 <0.0000000000000002 ***
-#CONTINENTEurope                                     -18.491242   0.107817 -171.506 <0.0000000000000002 ***
-#CONTINENTMadagascar                                   0.083484   0.766111    0.109               0.913    
-#CONTINENTMelanesia, Micronesia, and Polynesia       -19.099019   0.844930  -22.604 <0.0000000000000002 ***
-#CONTINENTSouth America                               17.050890   0.156352  109.055 <0.0000000000000002 ***
-#CONTINENTSoutheast Asia                              -6.517757   0.270310  -24.112 <0.0000000000000002 ***
-#---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#Residual standard error: 8.758 on 209646 degrees of freedom
-#  (35701 observations deleted due to missingness)
-#Multiple R-squared:  0.4021,	Adjusted R-squared:  0.4021 
-#F-statistic: 1.41e+04 on 10 and 209646 DF,  p-value: < 0.00000000000000022
-                     
-
-#Residual standard error: 10.94 on 245356 degrees of freedom
-#Multiple R-squared:  0.06773,	Adjusted R-squared:  0.06772 
-#F-statistic: 1.782e+04 on 1 and 245356 DF,  p-value: < 0.00000000000000022
-                                        
+# Examine top 3 models by AIC                     
+plot(model_9, which = 1)    
+pchisq(model_9$deviance, df = model_9$df.residual, lower.tail = FALSE)    
+summary(model_9)
+pR2(model_9)
+                                                         
+plot(model_8, which = 1)    
+pchisq(model_8$deviance, df = model_8$df.residual, lower.tail = FALSE)    
+summary(model_8)
+pR2(model_8)            
+                                                         
+plot(model_7, which = 1)    
+pchisq(model_8$deviance, df = model_8$df.residual, lower.tail = FALSE)    
+summary(model_8)
+pR2(model_8)                     
